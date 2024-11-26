10f889e3e26394f89554c3772fe772aeb4ace752
timoslater <tsla0307@gmail.com>
2024-11-26 11:01:43 -0500

    add tuning values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PinpointTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PinpointTest.java
@@ -58,7 +58,7 @@ For support, contact tech@gobilda.com
 -Ethan Doak
  */
 
-@TeleOp(name="goBILDA® PinPoint Odometry Example", group="Linear OpMode")
+@TeleOp(name="Pinpoint Test", group="Linear OpMode")
 //@Disabled
 
 public class PinpointTest extends LinearOpMode {
@@ -84,7 +84,7 @@ public class PinpointTest extends LinearOpMode {
         the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
         backwards is a negative number.
          */
-        odo.setOffsets(-84.0, -168.0); //these are tuned for 3110-0002-0001 Product Insight #1
+        odo.setOffsets(-73, 0); //these are tuned for 3110-0002-0001 Product Insight #1
 
         /*
         Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
@@ -101,7 +101,7 @@ public class PinpointTest extends LinearOpMode {
         increase when you move the robot forward. And the Y (strafe) pod should increase when
         you move the robot to the left.
          */
-        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
 
         /*
