194984ad910197cb2bf6b99b9dd0645c8d957808
timoslater <tsla0307@gmail.com>
2025-01-10 21:16:50 -0500

    --allow-empty

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/TeleOp2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/TeleOp2024.java
@@ -33,11 +33,12 @@ public class TeleOp2024 extends LinearOpMode {
     private IMU imu;
     private Servo hand;
     private Servo elbow;
-    private int lastPos;
+    private int lastPos = 325;
     private boolean isDropping = false;
 
-
     public boolean liftRunning = false;
+
+    public boolean positionSet = false;
 
     public void movement() {
         double modifier = 1;//nearBoard ? 0.65 : 1;
@@ -85,7 +86,7 @@ public class TeleOp2024 extends LinearOpMode {
         claw.setPosition(.3);
         if (!isDropping) {
             elbow.setPosition(0.296);
-            hand.setPosition(0.416);
+            hand.setPosition(0.92);
         }
 
     }
@@ -94,7 +95,7 @@ public class TeleOp2024 extends LinearOpMode {
         claw.setPosition(.64);
         if (!isDropping) {
             elbow.setPosition(0.38);
-            hand.setPosition(0.5);
+            hand.setPosition(0.94);
         };
     }
 
@@ -114,19 +115,20 @@ public class TeleOp2024 extends LinearOpMode {
 
     public void armPositionIdle() {
         isDropping = false;
-        hand.setPosition(.416);
+        hand.setPosition(.92);
         clawOpen();
     }
 
-    public void armPositionDrop() {
+    public void armPositionDrop() throws InterruptedException {
         isDropping = true;
-        hand.setPosition(1);
         elbow.setPosition(0);
+        Thread.sleep(500);
+        hand.setPosition(0);
     }
 
 
      @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         telemetry.addData("Status", "Initialized", "haggis");
 
         // Initialize the hardware variables. Note that the strings used here as parameters
@@ -184,40 +186,41 @@ public class TeleOp2024 extends LinearOpMode {
         while (opModeIsActive()) {
            //armControl.moveLiftTo(2400);
 
+            if (!positionSet && lift1.getCurrentPosition() > 250) {
+                positionSet = true;
+                armPositionIdle();
+            }
+
           movement();
 
           armGamepad = gamepad2.getGamepadId() == -1 ? gamepad1 : gamepad2;
 
           if (!liftRunning) {
-              if (armGamepad.left_trigger > 0 && (lift1.getCurrentPosition() < 5900  || resetting)) {
-                  liftUp(armGamepad.left_trigger*0.75);
-                  lastPos = lift1.getCurrentPosition();
-              } else if (armGamepad.right_trigger > 0) {
-                  if (lift1.getCurrentPosition() > 330 || resetting) {
-
-                      int ticksToLimit = Math.abs(lift1.getCurrentPosition() - 330);
-                      if (ticksToLimit < 200) {
-                          liftDown(armGamepad.right_trigger * (ticksToLimit - 200) / 200);
+                  if (armGamepad.left_trigger > 0 && (lift1.getCurrentPosition() < 5900 || resetting)) {
+                      liftUp(armGamepad.left_trigger * 0.75);
+                      lastPos = lift1.getCurrentPosition();
+                  } else if (armGamepad.right_trigger > 0 && (lift1.getCurrentPosition() > 230 || resetting)) {
+                      int ticksToLimit = Math.abs(lift1.getCurrentPosition() - 230);
+                      if (ticksToLimit < 300) {
+                          liftDown(armGamepad.right_trigger * (ticksToLimit - 300) / 300);
                       } else {
-                          liftDown(armGamepad.right_trigger * 0.75);
+                          liftDown(armGamepad.right_trigger * 0.5);
                       }
                       lastPos = lift1.getCurrentPosition();
                   } else {
-                      // lower elbow
-                  }
-              } else {
-                  int positionError = lastPos - lift1.getCurrentPosition();
-                  double kP = 0.25;
+                      int positionError = lastPos - lift1.getCurrentPosition();
+                      double correctionPower;
+                      double kP = 0.25;
 
-                  if (Math.abs(positionError) > 25) {
-                      double correctionPower = kP * (positionError / 50.0) * (positionError > 0 ? 1 : -1);
-                      lift1.setPower(correctionPower);
-                      lift2.setPower(correctionPower);
-                  } else {
-                      lift1.setPower(0);
-                      lift2.setPower(0);
+                      if (Math.abs(positionError) > 25) {
+                          correctionPower = kP * (positionError / 50.0) * (positionError > 0 ? 1 : -1);
+                          lift1.setPower(correctionPower);
+                          lift2.setPower(correctionPower);
+                      } else {
+                          lift1.setPower(0);
+                          lift2.setPower(0);
+                      }
                   }
-              }
           }
 
 
